5ae733325a8f1e4f69b5658e64feba5aeb6cf24a
Desiah Barnett <desiahbarnett@gmail.com>
2024-01-11 11:33:48 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(arrayOfDrivers, name) {
+    const matchingRecords = arrayOfDrivers.filter((element) => element.toLowerCase() === name.toLowerCase());
+    return matchingRecords; 
+}
+function fuzzyMatch(arrayOfDrivers, name) {
+    const matchingDrivers = arrayOfDrivers.filter((element) => name[0] === element [0])
+    return matchingDrivers;
+}
+function matchName(arrayOfDriverObjects, string) {
+    const newArray = arrayOfDriverObjects.filter((element) => element.name === string)
+    return newArray
+}
